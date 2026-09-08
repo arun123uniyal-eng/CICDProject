af9fde9438136c5cc15790d9b565f9e82f947172
Arun <arun123uniyal@gmail.com>
2026-09-08 08:40:43 +0530

    Initial commit with FastAPI, Docker, and GitHub Actions CI/CD

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI 
+app = FastAPI() 
+@app.get("/") 
+def home(): 
+    return {"message": "Welcome to cicdproject on Windows!"} 
+@app.get("/health") 
+def health(): 
+    return {"status": "healthy"} 
